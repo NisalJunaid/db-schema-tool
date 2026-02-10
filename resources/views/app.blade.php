54d164472f4cf9e475bfb21287fcfbf44e6e4ce8
NisalJunaid <105519086+NisalJunaid@users.noreply.github.com>
2026-02-11 01:46:58 +0500

    Fix Font Awesome v6 CDN and toolbar icon classes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,6 @@
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-            integrity="sha512-SnH5WK+bZxgPHs44uWix+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkR4j8WcG4P5F3aQ7AyYQfA5hcbfM8A7m3xg=="
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
